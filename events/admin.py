5f4b5a40e42c85c4d6ece8445225a85218ae93bc
Greg Kirkpatrick <gkirkpatrick@users.noreply.github.com>
2025-08-08 16:05:16 -0400

    Add tracking fields to Source and expose via API

--- events/admin.py
+++ events/admin.py
@@ -4,9 +4,24 @@ from .models import Source, Event, ServiceToken
 
 @admin.register(Source)
 class SourceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'base_url', 'status', 'last_crawl')
-    fields = ('user', 'name', 'base_url', 'search_method', 'status', 'event', 'last_crawl')
-    readonly_fields = ('last_crawl',)
+    list_display = (
+        'name',
+        'base_url',
+        'status',
+        'last_run_at',
+        'date_added',
+    )
+    fields = (
+        'user',
+        'name',
+        'base_url',
+        'search_method',
+        'status',
+        'event',
+        'last_run_at',
+        'date_added',
+    )
+    readonly_fields = ('last_run_at', 'date_added')
 
 
 @admin.register(Event)
